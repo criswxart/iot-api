cf00b6633d82e0f4d396c9f2753b5b02eb626a5f
Cristian <criswxart@gmail.com>
2025-03-05 18:02:47 -0300

    primer commit

---
+++ iot-api/src/main/java/com/tld/model/Location.java
@@ -0,0 +1,32 @@
+package com.tld.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "location")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(nullable = false)
+    private String locationName;
+
+    @Column(nullable = false)
+    private String locationCountry;
+
+    @Column(nullable = false)
+    private String locationCity;
+
+    private String locationMeta;
+}
