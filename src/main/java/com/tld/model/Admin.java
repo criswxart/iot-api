6fb4a8b0bd31738a35d07c30b1f1e805ed3e2c25
Cristian <criswxart@gmail.com>
2025-03-05 18:21:53 -0300

    mover raiz

---
+++ src/main/java/com/tld/model/Admin.java
@@ -0,0 +1,22 @@
+package com.tld.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/tld/model/Company.java
@@ -0,0 +1,22 @@
+package com.tld.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "company")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String companyName;
+
+    @Column(nullable = false, unique = true)
+    private String companyApiKey;
+}
+
