d1db4b81247f70502f8389ea90afe59f4f205c0f
sebastian <sebasitanruizbartsch@gmail.com>
2025-03-06 00:36:22 -0300

    Descripción de los cambios realizados

---
+++ src/main/java/com/tld/entity/User.java
@@ -0,0 +1,22 @@
+package com.tld.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String userPassword;
+}
