dac40d77361e4786b05092064d899a15fdb4b957
sebastian-bartsch <sebastianruizbartsch@gmail.com>
2025-03-17 11:13:53 -0300

    corrijo linea 34 donde se concatenaba "role"+

--- src/main/java/com/tld/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/tld/service/impl/UserDetailsServiceImpl.java
@@ -31,7 +31,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         for (Role role : user.get().getRole()) {
 //            grantedAuthorities.add(new SimpleGrantedAuthority(role.getRoleName()));
-        	grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+        	grantedAuthorities.add(new SimpleGrantedAuthority(role.getRoleName()));
             for (Permission permission : role.getPermissions()) {
                 grantedAuthorities.add(new SimpleGrantedAuthority(permission.getPermissionName()));
             }
